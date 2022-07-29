7c5f0863da65bea47cf11edd95ea21edf882079f
Neoxy <1007050740@qq.com>
2022-07-29 21:34:21 +0800

    Add files via upload

---
+++ logger.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int logInfo(char* info);
+int logWarning(char* warning);
+int logError(char* error);
+
+int logInfo(char* info) {
+	printf("[info]");
+	printf(info);
+	printf("\n");
+	return 0;
+}
+
+int logWarning(char* warning){
+	printf("[warning]");
+	printf(warning);
+	printf("\n");
+	return 0;
+}
+
+int logError(char* error) {
+	printf("[error]");
+	printf(error);
+	printf("\n");
+	return 0;
+}
---
+++ logger.h
@@ -0,0 +1,3 @@
+int logInfo(char* info);
+int logWarning(char* warning);
+int logError(char* error);
